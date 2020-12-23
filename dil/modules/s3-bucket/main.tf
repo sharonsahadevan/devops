4a8c533735a816333a4f6b1cd5cb374d6ece5b0d
sharon <sharonsujee36@gmail.com>
2020-12-23 22:51:11 +0530

    S3 bucket policy

--- dil/modules/s3-bucket/main.tf
+++ dil/modules/s3-bucket/main.tf
@@ -1,17 +1,72 @@
-provider "aws" {
-  region = "eu-central-1"
+
+locals {
+  dil_data_bucket            = "dil-demo-22323"
+  dil_terraform_state_bucket = "dil-demo2-7262"
 }
 
-module "s3-bucket" {
-  source              = "terraform-aws-modules/s3-bucket/aws"
-  version             = "1.17.0"
-  bucket              = "dil-test-bucket"
-  block_public_policy = true
-  force_destroy       = true
+
+data "aws_iam_policy_document" "dil_data_bucket_policy" {
+  statement {
+    actions = [
+      "s3:*",
+    ]
+    effect = "Deny"
+    resources = [
+      "arn:aws:s3:::${local.dil_data_bucket}",
+    ]
+
+    condition {
+      test     = "StringNotLike"
+      variable = "aws:SourceVpce"
+
+      values = [
+        "${module.vpc.vpc_endpoint_s3_id}",
+
+      ]
+    }
+  }
+}
+
+
+
+
+
+module "dil_data_bucket" {
+  source                  = "terraform-aws-modules/s3-bucket/aws"
+  version                 = "1.17.0"
+  bucket                  = local.dil_data_bucket
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+  force_destroy           = true
+  policy                  = data.aws_iam_policy_document.dil_data_bucket_policy.json
+
   tags = {
     "Terraform"   = "true",
     "Environment" = "dev"
 
   }
 }
+
+module "dil_terraform_state_bucket" {
+  source                  = "terraform-aws-modules/s3-bucket/aws"
+  version                 = "1.17.0"
+  bucket                  = "dil-s3-test-bucket-terraform"
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+  force_destroy           = true
+
+  policy = data.aws_iam_policy_document.dil_data_bucket_policy.json
+
+  tags = {
+    "Terraform"   = "true",
+    "Environment" = "dev"
+
+  }
+}
+
+
 
--- dil/modules/s3-bucket/outputs.tf
+++ dil/modules/s3-bucket/outputs.tf
@@ -1,4 +1,17 @@
-output "this_s3_bucket_id" {
+output "dil_terraform_state_bucket" {
   description = " The name of the bucket."
-  value       = module.s3-bucket.this_s3_bucket_id
+  value       = module.dil_terraform_state_bucket.this_s3_bucket_id
 }
+
+output "dil_data_bucket" {
+  description = " The name of the bucket."
+  value       = module.dil_data_bucket.this_s3_bucket_id
+}
+
+
+
+output "vpc_s3_endpoint" {
+
+  value = module.vpc.vpc_endpoint_s3_id
+}
+
