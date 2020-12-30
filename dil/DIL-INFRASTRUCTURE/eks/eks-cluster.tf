365f9daa6057e729b46025152c131dd7c4b90dc9
sharon <sharonsujee36@gmail.com>
2020-12-30 16:07:31 +0530

    alb-policy

--- dil/DIL-INFRASTRUCTURE/eks/eks-cluster.tf
+++ dil/DIL-INFRASTRUCTURE/eks/eks-cluster.tf
@@ -16,7 +16,7 @@ module "eks" {
     {
       name                          = "worker-group-1"
       instance_type                 = "t2.micro"
-      asg_desired_capacity          = 1
+      asg_desired_capacity          = 2
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
     },
     {
--- dil/DIL-INFRASTRUCTURE/eks/oidc-iam-role.tf
+++ dil/DIL-INFRASTRUCTURE/eks/oidc-iam-role.tf
@@ -1,13 +1,13 @@
-module "iam_iam-assumable-role-with-oidc" {
-  source                = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version               = "3.6.0"
-  aws_account_id        = var.aws_account_id
-  role_name             = "iam-test-sa-role"
-  create_role           = true
-  force_detach_policies = true
-  provider_url          = module.eks.cluster_oidc_issuer_url
-  role_policy_arns      = ["arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"]
-}
+# module "iam_iam-assumable-role-with-oidc" {
+#   source                = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+#   version               = "3.6.0"
+#   aws_account_id        = var.aws_account_id
+#   role_name             = "iam-test-sa-role"
+#   create_role           = true
+#   force_detach_policies = true
+#   provider_url          = module.eks.cluster_oidc_issuer_url
+#   role_policy_arns      = ["arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"]
+#}
 
 module "alb_ingress_iam-assumable-role-with-oidc" {
   source                = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
@@ -17,17 +17,17 @@ module "alb_ingress_iam-assumable-role-with-oidc" {
   create_role           = true
   force_detach_policies = true
   provider_url          = module.eks.cluster_oidc_issuer_url
-  role_policy_arns      = ["arn:aws:iam::289714961096:policy/alb-test-sa-policy"]
+  role_policy_arns      = [module.alb_iam_policy.arn]
 }
 
 
-module "traefik_ingress_iam-assumable-role-with-oidc" {
-  source                = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version               = "3.6.0"
-  aws_account_id        = var.aws_account_id
-  role_name             = "traefik-ingress-controller"
-  create_role           = true
-  force_detach_policies = true
-  provider_url          = module.eks.cluster_oidc_issuer_url
-  role_policy_arns      = ["arn:aws:iam::289714961096:policy/traefik-test-sa-policy"]
-}
+# module "traefik_ingress_iam-assumable-role-with-oidc" {
+#   source                = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+#   version               = "3.6.0"
+#   aws_account_id        = var.aws_account_id
+#   role_name             = "traefik-ingress-controller"
+#   create_role           = true
+#   force_detach_policies = true
+#   provider_url          = module.eks.cluster_oidc_issuer_url
+#   role_policy_arns      = ["arn:aws:iam::289714961096:policy/traefik-test-sa-policy"]
+# }
